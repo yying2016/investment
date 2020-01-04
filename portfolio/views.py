5b58ea1c6e4469bbb08741d7a0ac6014eede0f3b
Ying ZHANG <zhangying19942014@hotmail.com>
2020-01-04 15:45:00 +0800

    first commit

---
+++ portfolio/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
